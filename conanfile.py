f15c124e80829f0d1b71e88c5f14df3a482217b4
bincrafters-user <bincrafters@gmail.com>
2019-11-15 04:54:21 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.14.0

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ class PangoConan(ConanFile):
         if not tools.which("pkg-config"):
             self.build_requires("pkg-config_installer/0.29.2@bincrafters/stable")
         if not tools.which("meson"):
-            self.build_requires("meson_installer/0.50.0@bincrafters/stable")
+            self.build_requires("meson/0.52.0")
 
     def requirements(self):
         self.requires("freetype/2.10.0")
