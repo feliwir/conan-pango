3f5fcf4284672ce9f31dc8411dfec48b5efaacd6
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-01-08 10:12:49 +0100

    upgrade meson

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ class PangoConan(ConanFile):
         if not tools.which("pkg-config"):
             self.build_requires("pkg-config_installer/0.29.2@bincrafters/stable")
         if not tools.which("meson"):
-            self.build_requires("meson/0.52.0")
+            self.build_requires("meson/0.53.0")
 
     def requirements(self):
         self.requires("freetype/2.10.0")
